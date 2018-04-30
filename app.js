53d03db2d5550427b71149d5bbce06342267b6fc
Mark Brown <bethanybeachbummer@yahoo.com>
2018-04-30 17:50:20 +0000

    April 30 2:00PM save - authentication

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var express 			= require("express"),
 	app 				= express(),
 	bodyParser 			= require("body-parser"),
 	mongoose 			= require("mongoose"),
+	passport    		= require("passport"),
+    LocalStrategy   	= require("passport-local"),
+    User            	= require("./models/user"),
 	Ticket 				= require("./models/ticket"),
 	Player				= require("./models/player"),
 	Comment				= require("./models/comment"),
@@ -18,6 +21,19 @@ mongoose.connect("mongodb://localhost/lotteryDB");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Wilson wins the cutest dog",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // ******************************************
 // this is the creation of the landing page
@@ -255,6 +271,16 @@ app.post("/players/:id/comments", function(req, res){
 	//connect new comment to campground
 	//redirect to campground show page
 });
+
+// ============
+//  AUTH ROUTES
+// ============
+
+// show register form
+app.get("/register", function(request, res){
+	res.render("register");
+});
+
 
 
 // ******************************************
